8a36d1dd1dafc1b09cf1d3e86429a458cbfe3741
xiaoyili <lixy50@qq.com>
2025-06-28 01:23:07 +0800

    feature:update script

--- update_my_notes.sh
+++ update_my_notes.sh
@@ -1,5 +1,5 @@
 python3 generate_recent_notes.py /Users/lixiaoyi/Documents/obsidian/xiaoyili/deploy
-npx quartz build
+npm run quartz -- build
 rm -rf ~/Documents/xiaoyi.space/deploy/*
 cp -a -f ./public/. ~/Documents/xiaoyi.space/deploy/
 rm /Users/lixiaoyi/Documents/obsidian/xiaoyili/deploy/最近更新.md
