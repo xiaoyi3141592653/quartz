38a9fc83060445f74e81774c5c2c691906689454
xiaoyili <lixy50@qq.com>
2025-12-13 01:35:24 +0800

    feature:adjust path on win

--- update_my_notes_win_sp9.sh
+++ update_my_notes_win_sp9.sh
@@ -1,5 +1,5 @@
-python3 ./generate_recent_notes.py /mnt/c/Users/lixy5/OneDrive/Documents/obsidian/xiaoyili/deploy
+python3 ./generate_recent_notes.py /mnt/c/Users/lixy5/Documents/xiaoyili-obsidian/deploy
 npx quartz build
 rm -rf /mnt/c/Users/lixy5/Documents/xiaoyi_space/deploy/*
 cp -a -f ./public/. /mnt/c/Users/lixy5/Documents/xiaoyi_space/deploy/
-rm -rf /mnt/c/Users/lixy5/OneDrive/Documents/obsidian/xiaoyili/deploy/最近更新.md
+rm -rf /mnt/c/Users/lixy5/Documents/xiaoyili-obsidian/deploy/最近更新.md
